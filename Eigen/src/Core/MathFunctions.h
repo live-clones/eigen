c094da6a2e281a0b8cfbc601142d0f7790a09e82
Charlie Schlosser <cs.schlosser@gmail.com>
2023-09-28 22:43:41 -0400

    cooking with gas

--- Eigen/src/Core/MathFunctions.h
+++ Eigen/src/Core/MathFunctions.h
@@ -1343,18 +1343,33 @@ double ceil(const double &x) { return ::ceil(x); }
 
 /** Log base 2 for 32 bits positive integers.
   * Conveniently returns 0 for x==0. */
-inline int log2(int x)
-{
-  eigen_assert(x>=0);
-  unsigned int v(x);
-  static const int table[32] = { 0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30, 8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31 };
-  v |= v >> 1;
-  v |= v >> 2;
-  v |= v >> 4;
-  v |= v >> 8;
-  v |= v >> 16;
-  return table[(v * 0x07C4ACDDU) >> 27];
+inline uint32_t log2(uint32_t x) {
+  EIGEN_ALIGN_MAX static constexpr uint8_t table[32] = {0, 9,  1,  10, 13, 21, 2,  29, 11, 14, 16, 18, 22, 25, 3, 30,
+                                     8, 12, 20, 28, 15, 17, 24, 7,  19, 27, 23, 6,  26, 5,  4, 31};
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+  return table[(x * 0x07C4ACDD) >> 27];
 }
+
+inline uint64_t log2(uint64_t value) {
+  EIGEN_ALIGN_MAX static constexpr uint8_t table[64] = {63, 0,  58, 1,  59, 47, 53, 2,  60, 39, 48, 27, 54, 33, 42, 3,
+                                     61, 51, 37, 40, 49, 18, 28, 20, 55, 30, 34, 11, 43, 14, 22, 4,
+                                     62, 57, 46, 52, 38, 26, 32, 41, 50, 36, 17, 19, 29, 10, 13, 21,
+                                     56, 45, 25, 31, 35, 16, 9,  12, 44, 24, 15, 8,  23, 7,  6,  5};
+  value |= value >> 1;
+  value |= value >> 2;
+  value |= value >> 4;
+  value |= value >> 8;
+  value |= value >> 16;
+  value |= value >> 32;
+  return table[(((value - (value >> 1)) * 0x07EDD5E59A4E28C2)) >> 58];
+}
+
+inline int log2(int x) { return log2(uint32_t(x)); }
+inline int64_t log2(int64_t x) { return log2(uint64_t(x)); }
 
 /** \returns the square root of \a x.
   *
--- Eigen/src/ThreadPool/SimpleThreadPoolDevice.h
+++ Eigen/src/ThreadPool/SimpleThreadPoolDevice.h
@@ -15,29 +15,36 @@ namespace Eigen {
 struct SimpleThreadPoolDevice {
   SimpleThreadPoolDevice(ThreadPool& pool) : m_pool(pool) {}
 
-  void parallelFor(Index begin, Index end, Index incr, std::function<void(Index)> f, Index minJobs) {
-    Index numJobs = end - begin;
-    if (numJobs > minJobs) {
-      Index mid = begin + (numJobs / 2);
-      mid = incr * (mid / incr); // mid & (-incr)
+  using Task = std::function<void()>;
+  using LinearFunctor = std::function<void(Index)>;
 
-      std::function<void()> fLeft =  [this, begin, mid, incr, f, minJobs]() { parallelFor(begin, mid, incr, f, minJobs); };
-      std::function<void()> fRight = [this,   mid, end, incr, f, minJobs]() { parallelFor(  mid, end, incr, f, minJobs); };
-
-      pool().Schedule(fLeft);
-      pool().Schedule(fRight);
+  void parallelFor(Index begin, Index end, Index incr, LinearFunctor f, Barrier& barrier, Index maxDepth, Index depth) {
+    eigen_assert((incr & (incr - 1)) == 0 && "this method specialized for power-of-two increments");
+    if (depth < maxDepth)
+    {
+      Index size = end - begin;
+      Index mid = begin + (size / 2);
+      mid &= -incr; // soooooo fancy!
+      Task left =  [this, begin, mid, incr, f, &barrier, maxDepth, depth]() { parallelFor(begin, mid, incr, f, barrier, maxDepth, depth + 1); };
+      Task right = [this,   mid, end, incr, f, &barrier, maxDepth, depth]() { parallelFor(  mid, end, incr, f, barrier, maxDepth, depth + 1); };
+      pool().Schedule(left);
+      pool().Schedule(right);
     }
     else
     {
-      for (Index index = begin; index + incr <= end; index += incr) {
-        f(index);
-      }
+      for (Index i = begin; i + incr <= end; i += incr) f(i);
+      barrier.Notify();
     }
   }
 
-  void initParallelFor(Index begin, Index end, Index incr, std::function<void(Index)> f) {
-    Index minJobs = 50;// for testing
-    parallelFor(begin, end, incr, f, minJobs);
+  void initParallelFor(Index begin, Index end, Index incr, LinearFunctor f) {
+    Index minSize = 50; // for testing
+    Index size = end - begin;
+    Index maxDepth = size <= minSize ? 0 : numext::log2(size) - numext::log2(minSize);
+    uint32_t numNotifies = 1 << static_cast<uint32_t>(maxDepth);
+    Barrier barrier(numNotifies);
+    parallelFor(begin, end, incr, f, barrier, maxDepth, 0);
+    barrier.Wait();
   }
   ThreadPool& pool() { return m_pool; }
 
@@ -47,42 +54,50 @@ struct SimpleThreadPoolDevice {
 namespace internal
 {
 
-// specialize what you want
-// Linear Vector Traversal -- the workhorse of Eigen assignments
 template <typename Kernel>
 struct dense_assignment_loop_with_device<Kernel, SimpleThreadPoolDevice, LinearVectorizedTraversal, NoUnrolling> {
+  typedef typename Kernel::Scalar Scalar;
+  typedef typename Kernel::PacketType PacketType;
+  enum {
+    requestedAlignment = Kernel::AssignmentTraits::LinearRequiredAlignment,
+    packetSize = unpacket_traits<PacketType>::size,
+    dstIsAligned = int(Kernel::AssignmentTraits::DstAlignment) >= int(requestedAlignment),
+    dstAlignment =
+        packet_traits<Scalar>::AlignedOnScalar ? int(requestedAlignment) : int(Kernel::AssignmentTraits::DstAlignment),
+    srcAlignment = Kernel::AssignmentTraits::JointAlignment
+  };
+
   EIGEN_DEVICE_FUNC static EIGEN_STRONG_INLINE EIGEN_CONSTEXPR void run(Kernel& kernel,
                                                                         SimpleThreadPoolDevice& device) {
     const Index size = kernel.size();
-    typedef typename Kernel::Scalar Scalar;
-    typedef typename Kernel::PacketType PacketType;
-    enum {
-      requestedAlignment = Kernel::AssignmentTraits::LinearRequiredAlignment,
-      packetSize = unpacket_traits<PacketType>::size,
-      dstIsAligned = int(Kernel::AssignmentTraits::DstAlignment) >= int(requestedAlignment),
-      dstAlignment = packet_traits<Scalar>::AlignedOnScalar ? int(requestedAlignment)
-                                                            : int(Kernel::AssignmentTraits::DstAlignment),
-      srcAlignment = Kernel::AssignmentTraits::JointAlignment
-    };
     const Index alignedStart =
         dstIsAligned ? 0 : internal::first_aligned<requestedAlignment>(kernel.dstDataPtr(), size);
     const Index alignedEnd = alignedStart + ((size - alignedStart) / packetSize) * packetSize;
 
-    // should we thread this loop?
-    unaligned_dense_assignment_loop<dstIsAligned != 0>::run(kernel, 0, alignedStart);
+    Barrier barrier(dstIsAligned ? 2 : 3);
 
-    
-    // for (Index index = alignedStart; index < alignedEnd; index += packetSize)
-    //   kernel.template assignPacket<dstAlignment, srcAlignment, PacketType>(index);
-
-    std::function<void(Index)> f = [&kernel](Index index) {
-      kernel.template assignPacket<dstAlignment, srcAlignment, PacketType>(index);
+    std::function<void()> headLoopFunctor = [&, alignedStart]() {
+      unaligned_dense_assignment_loop<dstIsAligned != 0>::run(kernel, 0, alignedStart);
+      barrier.Notify();
     };
 
-    device.initParallelFor(alignedStart, alignedEnd, packetSize, f);
+    std::function<void()> vectorLoopFunctor = [&, alignedStart, alignedEnd]() {
+      std::function<void(Index)> vectorIterFunctor = [&](Index index) {
+        kernel.template assignPacket<dstAlignment, srcAlignment, PacketType>(index);
+      };
+      device.initParallelFor(alignedStart, alignedEnd, packetSize, vectorIterFunctor);
+      barrier.Notify();
+    };
 
-    // should we thread this loop?
-    unaligned_dense_assignment_loop<>::run(kernel, alignedEnd, size);
+    std::function<void()> tailLoopFunctor = [&, alignedEnd, size]() {
+      unaligned_dense_assignment_loop<>::run(kernel, alignedEnd, size);
+      barrier.Notify();
+    };
+
+    if (!dstIsAligned) device.pool().Schedule(headLoopFunctor);
+    device.pool().Schedule(vectorLoopFunctor);
+    device.pool().Schedule(tailLoopFunctor);
+    barrier.Wait();
   }
 };
 
