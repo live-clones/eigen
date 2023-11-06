0844f9e9a7b1a9cbddc38928c381cd4a00e268d9
Rasmus Munk Larsen <rmlarsen@google.com>
2023-11-06 15:40:42 -0800

    Revret accidental change.

--- test/threads_eventcount.cpp
+++ test/threads_eventcount.cpp
@@ -1,16 +1,16 @@
 // This file is part of Eigen, a lightweight C++ template library
 // for linear algebra.
 //
-// Copyright (C) 2023 Rasmus Munk Larsen <rmlarsen@google.com>
+// Copyright (C) 2016 Dmitry Vyukov <dvyukov@google.com>
+// Copyright (C) 2016 Benoit Steiner <benoit.steiner.goog@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #define EIGEN_USE_THREADS
-#ifdef EIGEN_GEMM_THREADPOOL
 #include "main.h"
-
+#include <Eigen/ThreadPool>
 
 // Visual studio doesn't implement a rand_r() function since its
 // implementation of rand() is already thread safe
