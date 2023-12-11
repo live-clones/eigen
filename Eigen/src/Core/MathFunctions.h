1a2f83e2c2704eaba2e57138af520fd2d9b404b3
Charlie Schlosser <cs.schlosser@gmail.com>
2023-12-10 22:55:35 -0500

    less terrible long double

--- Eigen/src/Core/MathFunctions.h
+++ Eigen/src/Core/MathFunctions.h
@@ -660,7 +660,7 @@ struct random_default_impl<long double, false, false> {
   static EIGEN_DEVICE_FUNC inline long double run(const long double& x, const long double& y) {
     return x + (y - x) * static_cast<long double>(impl::run(0.0, 1.0));
   }
-  static EIGEN_DEVICE_FUNC inline long double run() { static_cast<long double>(impl::run()); }
+  static EIGEN_DEVICE_FUNC inline long double run() { return static_cast<long double>(impl::run()); }
 };
 
 template <typename Scalar>
