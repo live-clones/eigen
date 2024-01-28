30f27352f6a8b6d02f76f8101815e88d1d5e0dc7
Charlie Schlosser <cs.schlosser@gmail.com>
2024-01-27 22:21:27 -0500

    disable true long double random for power pc

--- Eigen/src/Core/MathFunctions.h
+++ Eigen/src/Core/MathFunctions.h
@@ -818,7 +818,7 @@ struct random_default_impl<Scalar, false, false> {
   }
 };
 
-template <bool Specialize = sizeof(long double) == 2 * sizeof(uint64_t) && std::numeric_limits<long double>::is_iec559>
+template <bool Specialize = sizeof(long double) == 2 * sizeof(uint64_t) && !EIGEN_ARCH_PPC>
 struct random_longdouble_impl {
   enum : int {
     Size = sizeof(long double),
