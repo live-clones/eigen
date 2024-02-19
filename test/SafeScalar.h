69d8e6d89acd7f45a5a9e7cd31ac7621877b5830
Charlie Schlosser <cs.schlosser@gmail.com>
2024-02-18 22:18:06 -0500

    delete SafeScalar assignment operator, its redundant to the ctor

--- test/SafeScalar.h
+++ test/SafeScalar.h
@@ -4,12 +4,11 @@ template <typename T>
 class SafeScalar {
  public:
   SafeScalar() : initialized_(false) {}
+
   SafeScalar(const T& val) : val_(val), initialized_(true) {}
-  SafeScalar& operator=(const T& val) {
-    val_ = val;
-    initialized_ = true;
-    return *this;
-  }
+
+  template <typename Source>
+  explicit SafeScalar(const Source& val) : SafeScalar(static_cast<T>(val)) {}
 
   operator T() const {
     VERIFY(initialized_ && "Uninitialized access.");
@@ -20,9 +19,6 @@ class SafeScalar {
   explicit operator Target() const {
     return static_cast<Target>(this->operator T());
   }
-
-  template <typename Source>
-  explicit SafeScalar(const Source& val) : SafeScalar(static_cast<T>(val)) {}
 
  private:
   T val_;
