04aac911addf647d6f980a5fb8157de9302ba391
Tobias Wood <tobias@spinicist.org.uk>
2024-05-24 11:26:59 +0100

    Disabled block access. Tests pass

--- unsupported/test/cxx11_tensor_roll.cpp
+++ unsupported/test/cxx11_tensor_roll.cpp
@@ -16,11 +16,6 @@ using Eigen::Tensor;
 
 template <int DataLayout>
 static void test_simple_roll() {
-  Tensor<float, 2> small(2, 3);
-  small.setRandom();
-  Tensor<float, 2> smallr = small.roll(std::array<Index, 2>{1, 1});
-  std::cerr << "TENSOR\n" << small << "\nROLLED\n" << smallr << '\n';
-
   Tensor<float, 4, DataLayout> tensor(2, 3, 5, 7);
   tensor.setRandom();
 
@@ -64,7 +59,7 @@ static void test_simple_roll() {
     for (int j = 0; j < 3; ++j) {
       for (int k = 0; k < 5; ++k) {
         for (int l = 0; l < 7; ++l) {
-          VERIFY_IS_EQUAL(tensor((i + 2 - 3) % 2, (j + 3 - 2) % 3, (k + 5 - 1) % 5, l), rolled_tensor(i, j, k, l));
+          VERIFY_IS_EQUAL(tensor((i + 1) % 2, (j + 1) % 3, (k + 4) % 5, l), rolled_tensor(i, j, k, l));
         }
       }
     }
@@ -77,25 +72,25 @@ static void test_expr_roll(bool LValue) {
   tensor.setRandom();
 
   array<bool, 4> dim_roll;
-  dim_roll[0] = 0;
+  dim_roll[0] = 2;
   dim_roll[1] = 1;
-  dim_roll[2] = 2;
+  dim_roll[2] = 0;
   dim_roll[3] = 3;
 
-  Tensor<float, 4, DataLayout> expected(2, 3, 5, 7);
+  Tensor<float, 4, DataLayout> expected(tensor.dimensions());
   if (LValue) {
     expected.roll(dim_roll) = tensor;
   } else {
     expected = tensor.roll(dim_roll);
   }
 
-  Tensor<float, 4, DataLayout> result(2, 3, 5, 7);
+  Tensor<float, 4, DataLayout> result(tensor.dimensions());
 
   array<ptrdiff_t, 4> src_slice_dim;
-  src_slice_dim[0] = 2;
-  src_slice_dim[1] = 3;
+  src_slice_dim[0] = tensor.dimension(0);
+  src_slice_dim[1] = tensor.dimension(1);
   src_slice_dim[2] = 1;
-  src_slice_dim[3] = 7;
+  src_slice_dim[3] = tensor.dimension(3);
   array<ptrdiff_t, 4> src_slice_start;
   src_slice_start[0] = 0;
   src_slice_start[1] = 0;
@@ -104,7 +99,7 @@ static void test_expr_roll(bool LValue) {
   array<ptrdiff_t, 4> dst_slice_dim = src_slice_dim;
   array<ptrdiff_t, 4> dst_slice_start = src_slice_start;
 
-  for (int i = 0; i < 5; ++i) {
+  for (int i = 0; i < tensor.dimension(2); ++i) {
     if (LValue) {
       result.slice(dst_slice_start, dst_slice_dim).roll(dim_roll) = tensor.slice(src_slice_start, src_slice_dim);
     } else {
@@ -114,10 +109,10 @@ static void test_expr_roll(bool LValue) {
     dst_slice_start[2] += 1;
   }
 
-  VERIFY_IS_EQUAL(result.dimension(0), 2);
-  VERIFY_IS_EQUAL(result.dimension(1), 3);
-  VERIFY_IS_EQUAL(result.dimension(2), 5);
-  VERIFY_IS_EQUAL(result.dimension(3), 7);
+  VERIFY_IS_EQUAL(result.dimension(0), tensor.dimension(0));
+  VERIFY_IS_EQUAL(result.dimension(1), tensor.dimension(1));
+  VERIFY_IS_EQUAL(result.dimension(2), tensor.dimension(2));
+  VERIFY_IS_EQUAL(result.dimension(3), tensor.dimension(3));
 
   for (int i = 0; i < expected.dimension(0); ++i) {
     for (int j = 0; j < expected.dimension(1); ++j) {
@@ -131,7 +126,7 @@ static void test_expr_roll(bool LValue) {
 
   dst_slice_start[2] = 0;
   result.setRandom();
-  for (int i = 0; i < 5; ++i) {
+  for (int i = 0; i < tensor.dimension(2); ++i) {
     if (LValue) {
       result.slice(dst_slice_start, dst_slice_dim).roll(dim_roll) = tensor.slice(dst_slice_start, dst_slice_dim);
     } else {
