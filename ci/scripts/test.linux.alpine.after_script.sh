00276181d76530cbe48e3b18f5b18c64867079a1
Charlie Schlosser <cs.schlosser@gmail.com>
2024-12-15 23:43:10 -0500

    install libxslt in the before_script

--- ci/scripts/test.linux.alpine.after_script.sh
+++ ci/scripts/test.linux.alpine.after_script.sh
@@ -6,9 +6,6 @@ set -x
 rootdir=`pwd`
 cd ${EIGEN_CI_BUILDDIR}
 
-# Install xml processor.
-apk add libxslt
-
 # Generate test results.
 xsltproc ${rootdir}/ci/CTest2JUnit.xsl Testing/`head -n 1 < Testing/TAG`/Test.xml > "JUnitTestResults_$CI_JOB_ID.xml"
 
