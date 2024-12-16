128fe56ebb0cf909fcc59d66c5cbf650a9344f49
Charlie Schlosser <cs.schlosser@gmail.com>
2024-12-15 19:39:17 -0500

    add tests, remove old linux smoke tests

---
+++ ci/scripts/test.linux.alpine.after_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+# Enter build directory.
+rootdir=`pwd`
+cd ${EIGEN_CI_BUILDDIR}
+
+# Install xml processor.
+apt-get update -y
+apk add libxslt
+
+# Generate test results.
+xsltproc ${rootdir}/ci/CTest2JUnit.xsl Testing/`head -n 1 < Testing/TAG`/Test.xml > "JUnitTestResults_$CI_JOB_ID.xml"
+
+# Return to root directory.
+cd ${rootdir}
+
+set +x
