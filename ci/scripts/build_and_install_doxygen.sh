5d1d63d9a7aa94c54bc1ae5f1efa0c2dc2f4f2e4
C. Antonio Sanchez <cantonios@google.com>
2025-02-01 21:23:28 -0800

    disable other smoke tests

--- ci/scripts/build_and_install_doxygen.sh
+++ ci/scripts/build_and_install_doxygen.sh
@@ -3,5 +3,4 @@ cmake -B doxygen/.build -G Ninja                                 \
   -DCMAKE_CXX_COMPILER=${EIGEN_CI_CXX_COMPILER}                  \
   -DCMAKE_C_COMPILER=${EIGEN_CI_C_COMPILER}                      \
   doxygen
-cmake --build doxygen/.build
-mv doxygen/.build/bin/doxygen /usr/local/bin
+cmake --build doxygen/.build -t install
