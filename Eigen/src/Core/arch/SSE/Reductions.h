bffdc72a2a1d67759fe3380317719f60db63d0d6
Charlie Schlosser <cs.schlosser@gmail.com>
2025-05-25 22:03:27 -0400

    use faster Packet4f redux

--- Eigen/src/Core/arch/SSE/Reductions.h
+++ Eigen/src/Core/arch/SSE/Reductions.h
@@ -190,10 +190,16 @@ EIGEN_STRONG_INLINE bool predux_any(const Packet2l& x) {
 template <typename Op>
 struct sse_predux_common<Packet4f, Op> {
   static EIGEN_STRONG_INLINE float run(const Packet4f& a) {
-    Packet4f tmp;
-    tmp = Op::packetOp(a, _mm_shuffle_ps(a, a, _MM_SHUFFLE(0, 1, 2, 3)));
-    tmp = Op::packetOp(tmp, _mm_unpackhi_ps(tmp, tmp));
-    return _mm_cvtss_f32(tmp);
+    Packet4f tmp1, tmp2;
+#ifndef EIGEN_VECTORIZE_SSE3
+    tmp1 = _mm_movehdup_ps(a);
+#else
+    tmp1 = _mm_shuffle_ps(a, a, _MM_SHUFFLE(2, 3, 0, 1));  // [ C D | A B ]
+#endif
+    tmp2 = Op::packetOp(a, tmp1);
+    tmp1 = _mm_movehl_ps(tmp1, tmp2);  //  [   C   D | D+C C+D ]  // let the compiler avoid a mov by reusing shuf
+    tmp2 = Op::packetOp(tmp2, tmp1);
+    return _mm_cvtss_f32(tmp2);
   }
 };
 
