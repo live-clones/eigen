6099eca1401da47f3f44baa016d2032e2244fa39
Adam Cogdell <adamcogdell@google.com>
2025-06-03 18:06:08 +0000

    Fix fuzzer range error for scalar parity check.

--- unsupported/Eigen/src/SpecialFunctions/SpecialFunctionsImpl.h
+++ unsupported/Eigen/src/SpecialFunctions/SpecialFunctionsImpl.h
@@ -1455,7 +1455,8 @@ struct zeta_impl {
 
     if (q <= zero) {
       if (q == numext::floor(q)) {
-        if (x == numext::floor(x) && long(x) % 2 == 0) {
+        if (x == numext::floor(x) &&
+            numext::fmod(x, Scalar(2.0)) == Scalar(0.0)) {
           return maxnum;
         } else {
           return nan;
