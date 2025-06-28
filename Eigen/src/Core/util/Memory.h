5f1b42c9fb19a9d797a6fc34f8fe0007eb3d37fb
Charlie Schlosser <cs.schlosser@gmail.com>
2025-06-28 12:27:43 -0400

    unformat [ci skip]

--- Eigen/src/Core/util/Memory.h
+++ Eigen/src/Core/util/Memory.h
@@ -1344,15 +1344,11 @@ EIGEN_DEVICE_FUNC void destroy_at(T* p) {
  */
 #ifndef EIGEN_ASSUME_ALIGNED
 #if defined(__cpp_lib_assume_aligned) && (__cpp_lib_assume_aligned >= 201811L)
-#define EIGEN_ASSUME_ALIGNED(PTR, ALIGN_BYTES)         \
-  {                                                    \
-    PTR = std::assume_aligned<8 * (ALIGN_BYTES)>(PTR); \
-  }
+#define EIGEN_ASSUME_ALIGNED(PTR, ALIGN_BYTES) \
+  { PTR = std::assume_aligned<8 * (ALIGN_BYTES)>(PTR); }
 #elif EIGEN_HAS_BUILTIN(__builtin_assume_aligned)
-#define EIGEN_ASSUME_ALIGNED(PTR, ALIGN_BYTES)                                      \
-  {                                                                                 \
-    PTR = static_cast<decltype(PTR)>(__builtin_assume_aligned(PTR, (ALIGN_BYTES))); \
-  }
+#define EIGEN_ASSUME_ALIGNED(PTR, ALIGN_BYTES) \
+  { PTR = static_cast<decltype(PTR)>(__builtin_assume_aligned(PTR, (ALIGN_BYTES))); }
 #else
 #define EIGEN_ASSUME_ALIGNED(PTR, ALIGN_BYTES) /* do nothing */
 #endif
