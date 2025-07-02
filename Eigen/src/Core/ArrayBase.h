3bb762889d002085e12cdd108d1f07f196efd2df
Charlie Schlosser <cs.schlosser@gmail.com>
2025-07-01 23:09:39 -0400

    dont bother moving base = derived

--- Eigen/src/Core/ArrayBase.h
+++ Eigen/src/Core/ArrayBase.h
@@ -108,7 +108,7 @@ class ArrayBase : public DenseBase<Derived> {
     return derived();
   }
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE constexpr ArrayBase& operator=(Derived&& other) {
-    swap(derived(), other);
+    derived() = static_cast<const Derived&>(other);
     return *this;
   }
 
