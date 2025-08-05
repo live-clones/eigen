80d71a20217f9850e13cb8e7d0ef3f11c0b1a812
Charlie Schlosser <cs.schlosser@gmail.com>
2025-08-04 23:57:57 -0400

    squash

---
+++ Eigen/src/Core/RealView.h
@@ -0,0 +1,204 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2025 Charlie Schlosser <cs.schlosser@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef EIGEN_REALVIEW_H
+#define EIGEN_REALVIEW_H
+
+// IWYU pragma: private
+#include "./InternalHeaderCheck.h"
+
+namespace Eigen {
+
+namespace internal {
+
+template <typename Xpr>
+struct traits<RealView<Xpr>> : public traits<Xpr> {
+  template <typename T>
+  static constexpr int double_size(T size, bool times_two) {
+    int size_as_int = int(size);
+    if (size_as_int == Dynamic) return Dynamic;
+    return times_two ? (2 * size_as_int) : size_as_int;
+  }
+  using Base = traits<Xpr>;
+  using ComplexScalar = typename Base::Scalar;
+  using Scalar = typename NumTraits<ComplexScalar>::Real;
+  static constexpr int ActualPacketAccessBit = packet_traits<Scalar>::Vectorizable ? PacketAccessBit : 0;
+  static constexpr int ActualLvalueBit = is_lvalue<Xpr>::value ? LvalueBit : 0;
+  static constexpr int FlagMask = HereditaryBits | LinearAccessBit | ActualPacketAccessBit | ActualLvalueBit;
+  static constexpr int Flags = Base::Flags & FlagMask;
+  static constexpr bool IsRowMajor = Flags & RowMajorBit;
+  static constexpr int RowsAtCompileTime = double_size(Base::RowsAtCompileTime, !IsRowMajor);
+  static constexpr int ColsAtCompileTime = double_size(Base::ColsAtCompileTime, IsRowMajor);
+  static constexpr int SizeAtCompileTime = size_at_compile_time(RowsAtCompileTime, ColsAtCompileTime);
+  static constexpr int MaxRowsAtCompileTime = double_size(Base::MaxRowsAtCompileTime, !IsRowMajor);
+  static constexpr int MaxColsAtCompileTime = double_size(Base::MaxColsAtCompileTime, IsRowMajor);
+  static constexpr int MaxSizeAtCompileTime = size_at_compile_time(MaxRowsAtCompileTime, MaxColsAtCompileTime);
+};
+
+template <typename Xpr>
+struct evaluator<RealView<Xpr>> : public evaluator<Xpr> {
+  using XprType = RealView<Xpr>;
+  using Traits = traits<XprType>;
+  using ComplexScalar = typename Traits::ComplexScalar;
+  using Scalar = typename Traits::Scalar;
+  using BaseEvaluator = evaluator<Xpr>;
+  static constexpr bool IsRowMajor = Traits::IsRowMajor;
+  static constexpr int RowsAtCompileTime = Traits::RowsAtCompileTime;
+  static constexpr int ColsAtCompileTime = Traits::ColsAtCompileTime;
+  static constexpr int SizeAtCompileTime = Traits::SizeAtCompileTime;
+  static constexpr int MaxRowsAtCompileTime = Traits::MaxRowsAtCompileTime;
+  static constexpr int MaxColsAtCompileTime = Traits::MaxColsAtCompileTime;
+  static constexpr int MaxSizeAtCompileTime = Traits::MaxSizeAtCompileTime;
+  static constexpr int CoeffReadCost = BaseEvaluator::CoeffReadCost;
+  static constexpr int Flags = Traits::Flags;
+  static constexpr int Alignment = BaseEvaluator::Alignment;
+
+  EIGEN_DEVICE_FUNC explicit evaluator(XprType realView) : BaseEvaluator(realView.m_xpr) {}
+
+  constexpr EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Scalar coeff(Index row, Index col) const {
+    Index crow = IsRowMajor ? row : row / 2;
+    Index ccol = IsRowMajor ? col / 2 : col;
+    ComplexScalar ccoeff = BaseEvaluator::coeff(crow, ccol);
+    bool returnReal = (IsRowMajor ? col : row) % 2 == 0;
+    return returnReal ? numext::real(ccoeff) : numext::imag(ccoeff);
+  }
+
+  constexpr EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Scalar coeff(Index index) const {
+    Index cindex = index / 2;
+    ComplexScalar ccoeff = BaseEvaluator::coeff(cindex);
+    bool returnReal = index % 2 == 0;
+    return returnReal ? numext::real(ccoeff) : numext::imag(ccoeff);
+  }
+
+  constexpr EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Scalar& coeffRef(Index row, Index col) {
+    Index crow = IsRowMajor ? row : row / 2;
+    Index ccol = IsRowMajor ? col / 2 : col;
+    ComplexScalar& ccoeff = BaseEvaluator::coeffRef(crow, ccol);
+    bool returnReal = (IsRowMajor ? col : row) % 2 == 0;
+    return returnReal ? numext::real_ref(ccoeff) : numext::imag_ref(ccoeff);
+  }
+
+  constexpr EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Scalar& coeffRef(Index index) {
+    Index cindex = index / 2;
+    ComplexScalar& ccoeff = BaseEvaluator::coeffRef(cindex);
+    bool returnReal = index % 2 == 0;
+    return returnReal ? numext::real_ref(ccoeff) : numext::imag_ref(ccoeff);
+  }
+
+  template <int LoadMode, typename PacketType>
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE PacketType packet(Index row, Index col) const {
+    constexpr int RealPacketSize = unpacket_traits<PacketType>::size;
+    using ComplexPacket = typename find_packet_by_size<ComplexScalar, RealPacketSize / 2>::type;
+    EIGEN_STATIC_ASSERT((find_packet_by_size<ComplexScalar, RealPacketSize / 2>::value),
+                        MISSING COMPATIBLE COMPLEX PACKET TYPE)
+    eigen_assert(((IsRowMajor ? col : row) % 2 == 0) && "the inner index must be even");
+
+    Index crow = IsRowMajor ? row : row / 2;
+    Index ccol = IsRowMajor ? col / 2 : col;
+    ComplexPacket cpacket = BaseEvaluator::template packet<LoadMode, ComplexPacket>(crow, ccol);
+    return preinterpret<PacketType, ComplexPacket>(cpacket);
+  }
+
+  template <int LoadMode, typename PacketType>
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE PacketType packet(Index index) const {
+    constexpr int RealPacketSize = unpacket_traits<PacketType>::size;
+    using ComplexPacket = typename find_packet_by_size<ComplexScalar, RealPacketSize / 2>::type;
+    EIGEN_STATIC_ASSERT((find_packet_by_size<ComplexScalar, RealPacketSize / 2>::value),
+                        MISSING COMPATIBLE COMPLEX PACKET TYPE)
+    eigen_assert((index % 2 == 0) && "the index must be even");
+
+    Index cindex = index / 2;
+    ComplexPacket cpacket = BaseEvaluator::template packet<LoadMode, ComplexPacket>(cindex);
+    return preinterpret<PacketType, ComplexPacket>(cpacket);
+  }
+
+  template <int LoadMode, typename PacketType>
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE PacketType packetSegment(Index row, Index col, Index begin, Index count) const {
+    constexpr int RealPacketSize = unpacket_traits<PacketType>::size;
+    using ComplexPacket = typename find_packet_by_size<ComplexScalar, RealPacketSize / 2>::type;
+    EIGEN_STATIC_ASSERT((find_packet_by_size<ComplexScalar, RealPacketSize / 2>::value),
+                        MISSING COMPATIBLE COMPLEX PACKET TYPE)
+    eigen_assert(((IsRowMajor ? col : row) % 2 == 0) && "the inner index must be even");
+    eigen_assert((begin % 2 == 0) && (count % 2 == 0) && "begin and count must be even");
+
+    Index crow = IsRowMajor ? row : row / 2;
+    Index ccol = IsRowMajor ? col / 2 : col;
+    Index cbegin = begin / 2;
+    Index ccount = count / 2;
+    ComplexPacket cpacket = BaseEvaluator::template packetSegment<LoadMode, ComplexPacket>(crow, ccol, cbegin, ccount);
+    return preinterpret<PacketType, ComplexPacket>(cpacket);
+  }
+
+  template <int LoadMode, typename PacketType>
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE PacketType packetSegment(Index index, Index begin, Index count) const {
+    constexpr int RealPacketSize = unpacket_traits<PacketType>::size;
+    using ComplexPacket = typename find_packet_by_size<ComplexScalar, RealPacketSize / 2>::type;
+    EIGEN_STATIC_ASSERT((find_packet_by_size<ComplexScalar, RealPacketSize / 2>::value),
+                        MISSING COMPATIBLE COMPLEX PACKET TYPE)
+    eigen_assert((index % 2 == 0) && "the index must be even");
+    eigen_assert((begin % 2 == 0) && (count % 2 == 0) && "begin and count must be even");
+
+    Index cindex = index / 2;
+    Index cbegin = begin / 2;
+    Index ccount = count / 2;
+    ComplexPacket cpacket = BaseEvaluator::template packetSegment<LoadMode, ComplexPacket>(cindex, cbegin, ccount);
+    return preinterpret<PacketType, ComplexPacket>(cpacket);
+  }
+};
+
+}  // namespace internal
+
+// Generic API dispatcher
+template <typename Derived>
+class RealViewImpl;
+
+template <typename Xpr>
+class RealView : public RealViewImpl<Xpr> {
+  EIGEN_STATIC_ASSERT(NumTraits<typename Xpr::Scalar>::IsComplex, SCALAR MUST BE COMPLEX)
+ public:
+  using Nested = RealView;
+  EIGEN_DEVICE_FUNC explicit RealView(Xpr& xpr) : m_xpr(xpr) {}
+  EIGEN_DEVICE_FUNC constexpr Index rows() const noexcept { return Xpr::IsRowMajor ? m_xpr.rows() : 2 * m_xpr.rows(); }
+  EIGEN_DEVICE_FUNC constexpr Index cols() const noexcept { return Xpr::IsRowMajor ? 2 * m_xpr.cols() : m_xpr.cols(); }
+  EIGEN_DEVICE_FUNC constexpr Index size() const noexcept { return 2 * m_xpr.size(); }
+
+  template <typename OtherDerived>
+  EIGEN_DEVICE_FUNC RealView operator=(const DenseBase<OtherDerived>& other);
+
+ protected:
+  friend struct internal::evaluator<RealView<Xpr>>;
+  Xpr& m_xpr;
+};
+
+template <typename Xpr>
+class RealViewImpl : public internal::dense_xpr_base<RealView<Xpr>>::type {
+ public:
+  using Base = typename internal::dense_xpr_base<RealView<Xpr>>::type;
+};
+
+template <typename Derived>
+EIGEN_DEVICE_FUNC typename DenseBase<Derived>::RealViewReturnType DenseBase<Derived>::realView() {
+  return RealViewReturnType(derived());
+}
+
+template <typename Derived>
+EIGEN_DEVICE_FUNC typename DenseBase<Derived>::ConstRealViewReturnType DenseBase<Derived>::realView() const {
+  return ConstRealViewReturnType(derived());
+}
+
+template <typename Derived>
+template <typename OtherDerived>
+EIGEN_DEVICE_FUNC RealView<Derived> RealView<Derived>::operator=(const DenseBase<OtherDerived>& other) {
+  internal::call_assignment(*this, other.derived());
+  return *this;
+}
+
+}  // namespace Eigen
+
+#endif  // EIGEN_REALVIEW_H
---
+++ test/realview.cpp
@@ -0,0 +1,91 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2025 The Eigen Authors
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "main.h"
+#include "SafeScalar.h"
+#include "CustomComplex.h"
+
+template <typename T>
+void test_realview(const T&) {
+  using Scalar = typename T::Scalar;
+  using RealScalar = typename NumTraits<Scalar>::Real;
+
+  constexpr Index minRows = T::RowsAtCompileTime == Dynamic ? 1 : T::RowsAtCompileTime;
+  constexpr Index maxRows = T::MaxRowsAtCompileTime == Dynamic ? (EIGEN_TEST_MAX_SIZE / 2) : T::MaxRowsAtCompileTime;
+  constexpr Index minCols = T::ColsAtCompileTime == Dynamic ? 1 : T::ColsAtCompileTime;
+  constexpr Index maxCols = T::MaxColsAtCompileTime == Dynamic ? (EIGEN_TEST_MAX_SIZE / 2) : T::MaxColsAtCompileTime;
+
+  Index rows = internal::random<Index>(minRows, maxRows);
+  Index cols = internal::random<Index>(minCols, maxCols);
+  Index rowFactor = (NumTraits<Scalar>::IsComplex && !T::IsRowMajor) ? 2 : 1;
+  Index colFactor = (NumTraits<Scalar>::IsComplex && T::IsRowMajor) ? 2 : 1;
+  Index sizeFactor = NumTraits<Scalar>::IsComplex ? 2 : 1;
+
+  T A(rows, cols), B(rows, cols);
+
+  VERIFY(A.realView().rows() == rowFactor * A.rows());
+  VERIFY(A.realView().cols() == colFactor * A.cols());
+  VERIFY(A.realView().size() == sizeFactor * A.size());
+
+  RealScalar alpha = internal::random(RealScalar(1), RealScalar(2));
+  A.setRandom();
+  B = A;
+
+  VERIFY_IS_APPROX(A.matrix().squaredNorm(), A.realView().matrix().squaredNorm());
+
+  for (Index r = 0; r < rows; r++) {
+    for (Index c = 0; c < cols; c++) {
+      A.coeffRef(r, c) = A.coeff(r, c) * Scalar(alpha);
+    }
+  }
+  B.realView() *= alpha;
+  VERIFY_IS_APPROX(A, B);
+
+  alpha = internal::random(RealScalar(1), RealScalar(2));
+  A.setRandom();
+  B = A;
+
+  for (Index r = 0; r < rows; r++) {
+    for (Index c = 0; c < cols; c++) {
+      A.coeffRef(r, c) = A.coeff(r, c) / Scalar(alpha);
+    }
+  }
+  B.realView() /= alpha;
+  VERIFY_IS_APPROX(A, B);
+}
+
+template <typename Scalar, int Rows, int Cols, int MaxRows = Rows, int MaxCols = Cols>
+void test_realview_driver() {
+  using ColMajorMatrixType = Matrix<Scalar, Rows, Cols, ColMajor, MaxRows, MaxCols>;
+  using ColMajorArrayType = Array<Scalar, Rows, Cols, ColMajor, MaxRows, MaxCols>;
+  using RowMajorMatrixType = Matrix<Scalar, Rows, Cols, RowMajor, MaxRows, MaxCols>;
+  using RowMajorArrayType = Array<Scalar, Rows, Cols, RowMajor, MaxRows, MaxCols>;
+  test_realview(ColMajorMatrixType());
+  test_realview(ColMajorArrayType());
+  test_realview(RowMajorMatrixType());
+  test_realview(RowMajorArrayType());
+}
+
+template <typename ComplexScalar, int Rows, int Cols, int MaxRows = Rows, int MaxCols = Cols>
+void test_realview_driver_complex() {
+  using RealScalar = typename NumTraits<ComplexScalar>::Real;
+  test_realview_driver<RealScalar, Rows, Cols, MaxRows, MaxCols>();
+  test_realview_driver<ComplexScalar, Rows, Cols, MaxRows, MaxCols>();
+}
+
+EIGEN_DECLARE_TEST(realview) {
+  for (int i = 0; i < g_repeat; i++) {
+    CALL_SUBTEST_1((test_realview_driver_complex<std::complex<float>, Dynamic, Dynamic>()));
+    CALL_SUBTEST_2((test_realview_driver_complex<std::complex<float>, Dynamic, 17>()));
+    CALL_SUBTEST_3((test_realview_driver_complex<std::complex<float>, 17, Dynamic>()));
+    CALL_SUBTEST_4((test_realview_driver_complex<std::complex<float>, Dynamic, 7, 17, 7>()));
+    CALL_SUBTEST_5((test_realview_driver_complex<std::complex<float>, 7, Dynamic, 7, 10>()));
+    CALL_SUBTEST_6((test_realview_driver_complex<CustomComplex<SafeScalar<float>>, Dynamic, Dynamic>()));
+  }
+}
