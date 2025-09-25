89805e91b3e4a0e050a35e64383247f0f41a9454
Charlie Schlosser <cs.schlosser@gmail.com>
2025-09-24 22:28:14 -0400

    might as well try

--- ci/scripts/common.windows.before_script.ps1
+++ ci/scripts/common.windows.before_script.ps1
@@ -6,3 +6,5 @@ Get-Variable CMAKE* | Format-Table -Wrap
 
 # Run a custom before-script command.
 if ("${EIGEN_CI_BEFORE_SCRIPT}") { Invoke-Expression -Command "${EIGEN_CI_BEFORE_SCRIPT}" }
+
+choco install ninja -y
