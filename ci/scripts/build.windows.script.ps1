4ecc1d9cd53dabf31a2546dd23c4a0181782c2db
Charlie Schlosser <cs.schlosser@gmail.com>
2025-09-24 22:46:05 -0400

    try again

--- ci/scripts/build.windows.script.ps1
+++ ci/scripts/build.windows.script.ps1
@@ -1,14 +1,14 @@
 # Find Visual Studio installation directory.
-# $VS_INSTALL_DIR = &"${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
+$VS_INSTALL_DIR = &"${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
 
 # Run VCVarsAll.bat initialization script and extract environment variables.
 # http://allen-mack.blogspot.com/2008/03/replace-visual-studio-command-prompt.html
-# cmd.exe /c "`"${VS_INSTALL_DIR}\VC\Auxiliary\Build\vcvarsall.bat`" $EIGEN_CI_MSVC_ARCH -vcvars_ver=$EIGEN_CI_MSVC_VER & set" |
-#   foreach {
-#     if ($_ -match "=") {
-#       $v = $_.split("="); set-item -force -path "ENV:\$($v[0])" -value "$($v[1])"
-#     }
-#   }
+cmd.exe /c "`"${VS_INSTALL_DIR}\VC\Auxiliary\Build\vcvarsall.bat`" $EIGEN_CI_MSVC_ARCH -vcvars_ver=$EIGEN_CI_MSVC_VER & set" |
+  foreach {
+    if ($_ -match "=") {
+      $v = $_.split("="); set-item -force -path "ENV:\$($v[0])" -value "$($v[1])"
+    }
+  }
 
 # Create and enter build directory.
 $rootdir = Get-Location
