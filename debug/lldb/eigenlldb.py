e3d2796360506e1c3b11034be195f5b8c2e33de0
Joseph Prince Mathew <jmathew@dbuoy.com>
2025-10-09 12:44:05 -0400

    Add copyright

--- debug/lldb/eigenlldb.py
+++ debug/lldb/eigenlldb.py
@@ -3,6 +3,7 @@
 # for linear algebra.
 #
 # Copyright (C) 2021 Huang, Zhaoquan <zhaoquan2008@hotmail.com>
+# Copyright (C) 2025 Prince Mathew, Joseph <josephrpincemathew@outlook.com
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
