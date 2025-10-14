a58b75f2da7f5564548e9704dd5fb7f800a15273
Charlie Schlosser <cs.schlosser@gmail.com>
2025-10-13 23:06:58 -0400

    clean up linux script

--- ci/scripts/test.linux.script.sh
+++ ci/scripts/test.linux.script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ctest_cmd="ctest ${EIGEN_CI_CTEST_ARGS} --parallel ${NPROC} --output-on-failure --no-compress-output --build-noclean ${target}"
+exit_code=1
+max_retries=3
+
 set -x
 
 # Enter build directory.
@@ -13,32 +17,29 @@ elif [[ ${EIGEN_CI_CTEST_LABEL} ]]; then
   target="-L ${EIGEN_CI_CTEST_LABEL}"
 fi
 
-ctest_cmd="ctest ${EIGEN_CI_CTEST_ARGS} --parallel ${NPROC} --output-on-failure --no-compress-output --build-noclean ${target}"
+set +x
 
-exit_code=1
-
-# Run the initial CTest command
 echo "Running initial tests..."
 if ${ctest_cmd} -T test; then
   echo "Tests passed on the first attempt."
   exit_code=0
 else
-  max_retries=3
-  # Initial run failed, start retries
   echo "Initial tests failed with exit code $?. Retrying up to ${max_retries} times..."
   retry=1
   while [[ ${retry} -le ${max_retries} ]]; do
     echo "Attempting retry ${retry} of ${max_retries}..."
-    # Rerun only the tests that failed previously
     if ${ctest_cmd} --rerun-failed; then
       echo "Tests passed on retry."
       exit_code=42
-      break # Exit the retry loop on success
+      break
+    else
+      exit_code=$?
     fi
     ((retry++))
   done
 fi
 
+set -x
 
 # Return to root directory.
 cd ${rootdir}
