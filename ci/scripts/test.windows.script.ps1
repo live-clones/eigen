89634568cff53e659400bda28e9e0305ed01a266
Charlie Schlosser <cs.schlosser@gmail.com>
2025-10-19 13:32:05 -0400

    this works on my machine

--- ci/scripts/test.windows.script.ps1
+++ ci/scripts/test.windows.script.ps1
@@ -13,11 +13,11 @@ if (${EIGEN_CI_CTEST_REGEX}) {
   $target = "-L","${EIGEN_CI_CTEST_LABEL}"
 }
 
-$ctest_cmd = "ctest ${EIGEN_CI_CTEST_ARGS} --parallel ${NPROC} --output-on-failure --no-compress-output --build-noclean ${target}"
+$ctest_cmd = { ctest ${EIGEN_CI_CTEST_ARGS} --parallel ${NPROC} --output-on-failure --no-compress-output --build-noclean ${target} }
 
 Write-Host "Running initial tests..."
 
-& $ctest_cmd '-T test'
+& $ctest_cmd "-T test"
 $exit_code = $LASTEXITCODE
 
 if ($exit_code -eq 0) {
@@ -26,7 +26,7 @@ if ($exit_code -eq 0) {
 else {
   Write-Host "Initial tests failed with exit code $exit_code. Retrying up to $EIGEN_CI_CTEST_REPEAT times..."
 
-  & $ctest_cmd '--rerun-failed --repeat until-pass:' + "${EIGEN_CI_CTEST_REPEAT}"
+  & $ctest_cmd "--rerun-failed --repeat until-pass:${EIGEN_CI_CTEST_REPEAT}"
   $exit_code = $LASTEXITCODE
 
   if ($exit_code -eq 0) {
