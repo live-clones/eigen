154fd4ec6eb43585ab547c97c1785dfae64705a8
Antonio Sanchez <cantonios@google.com>
2025-10-27 21:41:20 +0000

    Fix MSVC CI.

--- ci/scripts/build.windows.script.ps1
+++ ci/scripts/build.windows.script.ps1
@@ -4,7 +4,7 @@ $VS_INSTALL_DIR = &"${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\v
 # Run VCVarsAll.bat initialization script and extract environment variables.
 # http://allen-mack.blogspot.com/2008/03/replace-visual-studio-command-prompt.html
 cmd.exe /c "`"${VS_INSTALL_DIR}\VC\Auxiliary\Build\vcvarsall.bat`" $EIGEN_CI_MSVC_ARCH -vcvars_ver=$EIGEN_CI_MSVC_VER & set" |
-  foreach {
+  ForEach-Object {
     if ($_ -match "=") {
       $v = $_.split("="); set-item -force -path "ENV:\$($v[0])" -value "$($v[1])"
     }
@@ -15,7 +15,7 @@ $rootdir = Get-Location
 if (-Not (Test-Path ${EIGEN_CI_BUILDDIR})) {
     mkdir $EIGEN_CI_BUILDDIR
 }
-cd $EIGEN_CI_BUILDDIR
+Set-Location $EIGEN_CI_BUILDDIR
 
 # We need to split EIGEN_CI_ADDITIONAL_ARGS, otherwise they are interpreted
 # as a single argument.  Split by space, unless double-quoted.
@@ -38,7 +38,7 @@ cmake --build . ${target} -- -k0 || cmake --build . ${target} -- -k0 -j1
 $success = $LASTEXITCODE
 
 # Return to root directory.
-cd ${rootdir}
+Set-Location ${rootdir}
 
 # Explicitly propagate exit code to indicate pass/failure of build command.
 if($success -ne 0) { Exit $success }
