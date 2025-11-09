bafe141d41ec683ff87a56677e3dac6fdaee71c9
Charlie Schlosser <cs.schlosser@gmail.com>
2025-11-09 09:23:55 -0500

    try another way

--- Eigen/src/Core/arch/LSX/PacketMath.h
+++ Eigen/src/Core/arch/LSX/PacketMath.h
@@ -2771,8 +2771,8 @@ EIGEN_STRONG_INLINE Packet16uc pdiv<Packet16uc>(const Packet16uc& a, const Packe
 }
 template <>
 EIGEN_STRONG_INLINE Packet16uc pabsdiff<Packet16uc>(const Packet16uc& a, const Packet16uc& b) {
-  Packet16c v = psub(preinterpret<Packet16c>(a), preinterpret<Packet16c>(b));
-  return preinterpret<Packet16uc>(pabs(v));
+  Packet16c v = psub(Packet16c(a), Packet16c(b));
+  return Packet16uc(pabs(v));
 }
 template <>
 EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Packet16uc pselect(const Packet16uc& mask, const Packet16uc& a,
@@ -2794,8 +2794,8 @@ EIGEN_STRONG_INLINE Packet16uc psqrt(const Packet16uc& a) {
 
 template <>
 EIGEN_STRONG_INLINE Packet8us pabsdiff<Packet8us>(const Packet8us& a, const Packet8us& b) {
-  Packet8s v = psub(preinterpret<Packet8s>(a), preinterpret<Packet8s>(b));
-  return preinterpret<Packet8us>(pabs(v));
+  Packet8s v = psub(Packet8s(a), Packet8s(b));
+  return Packet8us(pabs(v));
 }
 template <>
 EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Packet8us pselect(const Packet8us& mask, const Packet8us& a, const Packet8us& b) {
@@ -2816,8 +2816,8 @@ EIGEN_STRONG_INLINE Packet8us psqrt(const Packet8us& a) {
 
 template <>
 EIGEN_STRONG_INLINE Packet4ui pabsdiff<Packet4ui>(const Packet4ui& a, const Packet4ui& b) {
-  Packet4i v = psub(preinterpret<Packet4i>(a), preinterpret<Packet4i>(b));
-  return preinterpret<Packet4ui>(pabs(v));
+  Packet4i v = psub(Packet4i(a), Packet4i(b));
+  return Packet4ui(pabs(v));
 }
 template <>
 EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE Packet4ui pselect(const Packet4ui& mask, const Packet4ui& a, const Packet4ui& b) {
