1db9de6e510813d8b5998b94fb70306e0acf2f3d
Charlie Schlosser <cs.schlosser@gmail.com>
2025-12-16 16:41:00 -0500

    dont use forbidden instructions

--- Eigen/src/Core/arch/AVX/PacketMath.h
+++ Eigen/src/Core/arch/AVX/PacketMath.h
@@ -541,37 +541,16 @@ template <int N>
 EIGEN_STRONG_INLINE Packet4l plogical_shift_left(Packet4l a) {
   return _mm256_slli_epi64(a, N);
 }
-#ifdef EIGEN_VECTORIZE_AVX512FP16
 template <int N>
 EIGEN_STRONG_INLINE Packet4l parithmetic_shift_right(Packet4l a) {
+#ifdef EIGEN_VECTORIZE_AVX512VL
   return _mm256_srai_epi64(a, N);
-}
 #else
-template <int N>
-EIGEN_STRONG_INLINE std::enable_if_t<(N == 0), Packet4l> parithmetic_shift_right(Packet4l a) {
-  return a;
-}
-template <int N>
-EIGEN_STRONG_INLINE std::enable_if_t<(N > 0) && (N < 32), Packet4l> parithmetic_shift_right(Packet4l a) {
-  __m256i hi_word = _mm256_srai_epi32(a, N);
-  __m256i lo_word = _mm256_srli_epi64(a, N);
-  return _mm256_blend_epi32(hi_word, lo_word, 0b01010101);
-}
-template <int N>
-EIGEN_STRONG_INLINE std::enable_if_t<(N >= 32) && (N < 63), Packet4l> parithmetic_shift_right(Packet4l a) {
-  __m256i hi_word = _mm256_srai_epi32(a, 31);
-  __m256i lo_word = _mm256_shuffle_epi32(_mm256_srai_epi32(a, N - 32), (shuffle_mask<1, 1, 3, 3>::mask));
-  return _mm256_blend_epi32(hi_word, lo_word, 0b01010101);
-}
-template <int N>
-EIGEN_STRONG_INLINE std::enable_if_t<(N == 63), Packet4l> parithmetic_shift_right(Packet4l a) {
-  return _mm256_cmpgt_epi64(_mm256_setzero_si256(), a);
-}
-template <int N>
-EIGEN_STRONG_INLINE std::enable_if_t<(N < 0) || (N > 63), Packet4l> parithmetic_shift_right(Packet4l a) {
-  return parithmetic_shift_right<int(N & 63)>(a);
-}
+  Packet4l signbit = _mm256_cmpgt_epi64(_mm256_setzero_si256(), a);
+  return _mm256_or_si256(_mm256_slli_epi64(signbit, 64 - N), _mm256_srli_epi64(a, N));
 #endif
+}
+
 template <>
 EIGEN_STRONG_INLINE Packet4l pload<Packet4l>(const int64_t* from) {
   EIGEN_DEBUG_ALIGNED_LOAD return _mm256_load_si256(reinterpret_cast<const __m256i*>(from));
@@ -3078,11 +3057,21 @@ EIGEN_STRONG_INLINE Packet8ui puintdiv(const Packet8ui& a, uint32_t magic, int s
   return result;
 }
 
+// emulate _mm256_sra_epi64 if it is not available
+EIGEN_STRONG_INLINE __m256i eigen_mm256_sra_epi64(__m256i a, int shift) {
+#ifdef EIGEN_VECTORIZE_AVX512
+  return _mm256_sra_epi64(a, _mm_cvtsi32_si128(shift));
+#else
+  __m256i left_bits = _mm256_sll_epi64(_mm256_cmpgt_epi64(_mm256_setzero_si256(), a), _mm_cvtsi32_si128(64 - shift));
+  __m256i right_bits = _mm256_srl_epi64(a, _mm_cvtsi32_si128(shift));
+  return _mm256_or_si256(left_bits, right_bits);
+#endif
+}
+
 template <>
 EIGEN_STRONG_INLINE Packet8i psintdiv(const Packet8i& a, int32_t magic, int shift, bool sign) {
   constexpr int kShuffleMask = shuffle_mask<0, 2, 0, 2>::mask;
   const __m256i cst_magic = _mm256_set1_epi32(magic);
-  const __m128i cst_shift = _mm_cvtsi32_si128(shift);
   const __m256i cst_sign = _mm256_set1_epi32(sign ? -1 : 1);
 
   __m256i a_sgn = _mm256_srai_epi32(a, 31);
@@ -3090,11 +3079,11 @@ EIGEN_STRONG_INLINE Packet8i psintdiv(const Packet8i& a, int32_t magic, int shif
   __m256i a_evn = _mm256_unpacklo_epi32(a, a_sgn);
   __m256i a_odd = _mm256_unpackhi_epi32(a, a_sgn);
 
-  __m256i b_evn = _mm256_srai_epi64(_mm256_mul_epi32(a_evn, cst_magic), 32);
-  __m256i b_odd = _mm256_srai_epi64(_mm256_mul_epi32(a_odd, cst_magic), 32);
+  __m256i b_evn = parithmetic_shift_right<32>((Packet4l)_mm256_mul_epi32(a_evn, cst_magic));
+  __m256i b_odd = parithmetic_shift_right<32>((Packet4l)_mm256_mul_epi32(a_odd, cst_magic));
 
-  __m256i t_evn = _mm256_sra_epi64(_mm256_add_epi64(b_evn, a_evn), cst_shift);
-  __m256i t_odd = _mm256_sra_epi64(_mm256_add_epi64(b_odd, a_odd), cst_shift);
+  __m256i t_evn = eigen_mm256_sra_epi64(_mm256_add_epi64(b_evn, a_evn), shift);
+  __m256i t_odd = eigen_mm256_sra_epi64(_mm256_add_epi64(b_odd, a_odd), shift);
 
   __m256i result =
       _mm256_castps_si256(_mm256_shuffle_ps(_mm256_castsi256_ps(t_evn), _mm256_castsi256_ps(t_odd), kShuffleMask));
@@ -3124,8 +3113,8 @@ EIGEN_STRONG_INLINE Packet4ul pumuluh_4ul(Packet4ul a, Packet4ul b) {
 
 EIGEN_STRONG_INLINE Packet4l psmuluh_4l(Packet4l a, Packet4l b) {
   Packet4l result = (Packet4l)pumuluh_4ul((Packet4ul)a, (Packet4ul)b);
-  result = _mm256_sub_epi64(result, _mm256_and_si256(b, _mm256_srai_epi64(a, 63)));
-  result = _mm256_sub_epi64(result, _mm256_and_si256(a, _mm256_srai_epi64(b, 63)));
+  result = _mm256_sub_epi64(result, _mm256_and_si256(b, psignbit(a)));
+  result = _mm256_sub_epi64(result, _mm256_and_si256(a, psignbit(b)));
   return result;
 }
 
@@ -3150,9 +3139,9 @@ EIGEN_STRONG_INLINE Packet4l psintdiv(const Packet4l& a, int64_t magic, int shif
 
   Packet4l b = psmuluh_4l(a, cst_magic);
   WidePacket4l t = padd_wide_signed(b, a);
-  __m256i result = _mm256_sra_epi64(t.second, _mm_cvtsi32_si128(shift));
+  __m256i result = eigen_mm256_sra_epi64(t.second, shift);
   result = _mm256_or_si256(result, _mm256_sll_epi64(t.first, _mm_cvtsi32_si128(64 - shift)));
-  result = _mm256_sub_epi64(result, _mm256_srai_epi64(a, 63));
+  result = _mm256_sub_epi64(result, psignbit(a));
   result = _mm256_sub_epi64(_mm256_xor_si256(result, cst_sign), cst_sign);
   return result;
 }
