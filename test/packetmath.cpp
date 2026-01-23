c4cf228fa4c7f47d5ec3e4f6dcd8508f131d6f87
Charlie Schlosser <cs.schlosser@gmail.com>
2026-01-22 21:36:25 -0500

    skip log2 and cbrt denorms in ieee test

--- test/packetmath.cpp
+++ test/packetmath.cpp
@@ -1280,11 +1280,11 @@ std::enable_if_t<!NumTraits<Scalar>::IsComplex, void> packetmath_ieee_special_va
   run_ieee_cases<PacketTraits::HasSqrt, Scalar, Packet, EIGEN_ARCH_ARM>(sqrt_fun());
   // TODO(rmlarsen): See if we can fix rsqrt for denorms without wreaking performance.
   run_ieee_cases<PacketTraits::HasRsqrt, Scalar, Packet, true>(rsqrt_fun());
-  run_ieee_cases<PacketTraits::HasCbrt, Scalar, Packet>(cbrt_fun());
+  run_ieee_cases<PacketTraits::HasCbrt, Scalar, Packet, EIGEN_ARCH_ARM>(cbrt_fun());
   run_ieee_cases<PacketTraits::HasExp, Scalar, Packet>(exp_fun());
   run_ieee_cases<PacketTraits::HasExp, Scalar, Packet>(exp2_fun());
   run_ieee_cases<PacketTraits::HasLog, Scalar, Packet, EIGEN_ARCH_ARM>(log_fun());
-  run_ieee_cases<PacketTraits::HasLog, Scalar, Packet>(log2_fun());
+  run_ieee_cases<PacketTraits::HasLog, Scalar, Packet, EIGEN_ARCH_ARM>(log2_fun());
   run_ieee_cases<PacketTraits::HasExpm1, Scalar, Packet>(expm1_fun());
   run_ieee_cases<PacketTraits::HasLog1p, Scalar, Packet>(log1p_fun());
   run_ieee_cases<PacketTraits::HasSin, Scalar, Packet>(sin_fun());
