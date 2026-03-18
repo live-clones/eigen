5f8327246231f4eded3cc93e7f0132ca8610ee6c
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2026-03-18 22:46:55 +0200

    Use EIGEN_DIAGNOSTICS to suppress array-bounds warning for all C++ standards
    Replace EIGEN_IF_CONSTEXPR approach (only effective in C++17+) with
    EIGEN_DIAGNOSTICS push/pop to suppress the false-positive GCC
    -Warray-bounds warning. This works for C++14 as well, since the
    diagnostic pragmas are standard-agnostic.
    
    Keep value-initialization of stride arrays to prevent uninitialized reads.

--- unsupported/Eigen/src/Tensor/TensorContraction.h
+++ unsupported/Eigen/src/Tensor/TensorContraction.h
@@ -490,15 +490,16 @@ struct TensorContractionEvaluatorBase {
         if (dim_idx != i) {
           m_lhs_inner_dim_contiguous = false;
         }
-        EIGEN_IF_CONSTEXPR(internal::array_size<left_nocontract_t>::value > 1) {
-          if (nocontract_idx + 1 < internal::array_size<left_nocontract_t>::value) {
-            m_i_strides[nocontract_idx + 1] = m_i_strides[nocontract_idx] * eval_left_dims[i];
-          } else {
-            m_i_size = m_i_strides[nocontract_idx] * eval_left_dims[i];
-          }
+        // Suppress false-positive GCC -Warray-bounds warning at -O3 when
+        // left_nocontract_t has size 1 (the runtime check prevents OOB access).
+        EIGEN_DIAGNOSTICS(push)
+        EIGEN_DIAGNOSTICS_OFF(disable : 4789, ignored "-Warray-bounds")
+        if (nocontract_idx + 1 < internal::array_size<left_nocontract_t>::value) {
+          m_i_strides[nocontract_idx + 1] = m_i_strides[nocontract_idx] * eval_left_dims[i];
         } else {
           m_i_size = m_i_strides[nocontract_idx] * eval_left_dims[i];
         }
+        EIGEN_DIAGNOSTICS(pop)
         dim_idx++;
         nocontract_idx++;
       }
@@ -516,15 +517,14 @@ struct TensorContractionEvaluatorBase {
       }
       if (!contracting) {
         m_dimensions[dim_idx] = eval_right_dims[i];
-        EIGEN_IF_CONSTEXPR(internal::array_size<right_nocontract_t>::value > 1) {
-          if (nocontract_idx + 1 < internal::array_size<right_nocontract_t>::value) {
-            m_j_strides[nocontract_idx + 1] = m_j_strides[nocontract_idx] * eval_right_dims[i];
-          } else {
-            m_j_size = m_j_strides[nocontract_idx] * eval_right_dims[i];
-          }
+        EIGEN_DIAGNOSTICS(push)
+        EIGEN_DIAGNOSTICS_OFF(disable : 4789, ignored "-Warray-bounds")
+        if (nocontract_idx + 1 < internal::array_size<right_nocontract_t>::value) {
+          m_j_strides[nocontract_idx + 1] = m_j_strides[nocontract_idx] * eval_right_dims[i];
         } else {
           m_j_size = m_j_strides[nocontract_idx] * eval_right_dims[i];
         }
+        EIGEN_DIAGNOSTICS(pop)
         m_right_nocontract_strides[nocontract_idx] = rhs_strides[i];
         dim_idx++;
         nocontract_idx++;
@@ -545,15 +545,14 @@ struct TensorContractionEvaluatorBase {
       Index size = eval_left_dims[left];
       eigen_assert(size == eval_right_dims[right] && "Contraction axes must be same size");
 
-      EIGEN_IF_CONSTEXPR(internal::array_size<contract_t>::value > 1) {
-        if (i + 1 < static_cast<int>(internal::array_size<contract_t>::value)) {
-          m_k_strides[i + 1] = m_k_strides[i] * size;
-        } else {
-          m_k_size = m_k_strides[i] * size;
-        }
+      EIGEN_DIAGNOSTICS(push)
+      EIGEN_DIAGNOSTICS_OFF(disable : 4789, ignored "-Warray-bounds")
+      if (i + 1 < static_cast<int>(internal::array_size<contract_t>::value)) {
+        m_k_strides[i + 1] = m_k_strides[i] * size;
       } else {
         m_k_size = m_k_strides[i] * size;
       }
+      EIGEN_DIAGNOSTICS(pop)
       m_left_contracting_strides[i] = lhs_strides[left];
       m_right_contracting_strides[i] = rhs_strides[right];
 
