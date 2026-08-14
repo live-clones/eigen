68fcc96657467fd5f9a7e9367dfe0f581209d4c9
onalante-ebay <32563185-onalante-ebay@users.noreply.gitlab.com>
2026-08-14 16:48:44 -0700

    Simplify unsigned less-than

--- Eigen/src/Core/arch/NEON/PacketMath.h
+++ Eigen/src/Core/arch/NEON/PacketMath.h
@@ -1639,13 +1639,8 @@ EIGEN_STRONG_INLINE Packet2ul pcmp_lt<Packet2ul>(const Packet2ul& a, const Packe
 #if EIGEN_ARCH_ARM64
   return vcltq_u64(a, b);
 #else
-  uint32x4_t const a32 = vreinterpretq_u32_u64(a);
-  uint32x4_t const b32 = vreinterpretq_u32_u64(b);
-  uint64x2_t const hi_lt = vreinterpretq_u64_s64(vshrq_n_s64(vreinterpretq_s64_u32(vcltq_u32(a32, b32)), 32));
-  uint64x2_t const lo_lt = vandq_u64(
-      vreinterpretq_u64_s64(vshrq_n_s64(vreinterpretq_s64_u32(vceqq_u32(a32, b32)), 32)),
-      vreinterpretq_u64_s64(vmovl_s32(vreinterpret_s32_u32(vmovn_u64(vreinterpretq_u64_u32(vcltq_u32(a32, b32)))))));
-  return vorrq_u64(hi_lt, lo_lt);
+  uint64x2_t const flag = vshrq_n_u64(vreinterpretq_u64_u8(vdupq_n_u8(0xFF)), 1);
+  return vreinterpretq_u64_s64(vshrq_n_s64(vreinterpretq_s64_u64(vqaddq_u64(vqsubq_u64(b, a), flag)), 63));
 #endif
 }
 template <>
